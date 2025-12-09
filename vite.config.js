4bef58987c382e87aa90bb344e67669eea957f8c
Caleb Crawley <crawleyhost@gmail.com>
2025-12-09 18:24:46 -0500

    Add Vite config for Github Pages display

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'src',
+  // Set the base URL for deployment (important for GitHub Pages project sites)
+  base: '/UXDevPracticum/', // Replace 'your-repo-name' with your actual repository name
+  build: {
+    // Output the build files to the 'dist' folder outside of the 'src' root
+    outDir: '../dist',
+    // Empty the output directory before building
+    emptyOutDir: false,
+  },
 })
